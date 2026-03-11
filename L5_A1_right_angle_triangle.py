0fdd6c5474b24d9dbf382219285e2e640e8e4958
Divit Reddy Allu <lkuntla@googlemail.com>
2026-03-11 17:34:19 +0100

    Add files via upload

---
+++ L5_A1_right_angle_triangle.py
@@ -0,0 +1,7 @@
+print("this is the right angle triangle pattern for star: ")
+n=int(input("Enter how many rows you want: " ))
+for i in range (n):
+   for j in range(i+1):
+       print("* ", end="")
+   print()
+    
